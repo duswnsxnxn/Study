4a3aae18788f2d8419c9c05928c6779bdf116af8
duswnsxnxn <duswnsxnxn@gmail.com>
2024-02-15 01:08:01 +0900

    ãwar배포

---
+++ (김영한) 스프링 부트 - 핵심 원리와 활용/실습/server/src/main/java/hello/spring/HelloController.java
@@ -0,0 +1,14 @@
+package hello.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-spring")
+    public String helloSpring() {
+        System.out.println("HelloController.helloSpring");
+        return "Hello, Spring!";
+    }
+}
