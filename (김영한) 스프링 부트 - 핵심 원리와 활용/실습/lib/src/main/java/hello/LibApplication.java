58cee18d0e2561af9b2e74371a66ffa6847ee75d
duswnsxnxn <duswnsxnxn@gmail.com>
2024-02-21 22:28:57 +0900

    modify

--- (김영한) 스프링 부트 - 핵심 원리와 활용/실습/lib/src/main/java/hello/LibApplication.java
+++ (김영한) 스프링 부트 - 핵심 원리와 활용/실습/lib/src/main/java/hello/LibApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibApplication {
+public class  LibApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibApplication.class, args);
