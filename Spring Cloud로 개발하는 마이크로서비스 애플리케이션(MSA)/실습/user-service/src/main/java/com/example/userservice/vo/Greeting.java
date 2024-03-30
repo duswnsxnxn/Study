1c6fa07062e0d738d0be1b8ada9862c0d3de86b2
duswnsxnxn <duswnsxnxn@gmail.com>
2024-03-31 05:54:35 +0900

    value나 Enviroment 주입 대신 ConfigurationProperties로 변경

--- Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/vo/Greeting.java
+++ Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/vo/Greeting.java
@@ -4,14 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+@ConfigurationProperties(prefix = "greeting")
 public class Greeting {
 
-    @Value("${greeting.message}")
     private String message;
 }
