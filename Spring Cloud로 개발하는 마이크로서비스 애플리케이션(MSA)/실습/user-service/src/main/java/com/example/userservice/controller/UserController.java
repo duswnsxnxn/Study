eaee461332370a712114c6a5c83df683bdc433d4
duswnsxnxn <duswnsxnxn@gmail.com>
2024-03-31 05:55:08 +0900

    회원가입api 구현

--- Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final Environment env;
+    private final UserService userService;
     private final Greeting greeting;
 
     @GetMapping("/health_check")
@@ -22,7 +24,15 @@ public class UserController {
 
     @GetMapping("/welcome")
     public String welcome() {
-//        return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = modelMapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+        return "create user was called";
     }
 }
