fac04ea610b15be945d6b752d222fcf56b3a023e
duswnsxnxn <duswnsxnxn@gmail.com>
2024-03-31 05:17:33 +0900

    user-service 구축

---
+++ Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.jpa.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encryted_password");
+
+        userRepository.save(userEntity);
+
+        return null;
+    }
+}
