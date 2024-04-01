639362dadb1b34f0abc7230aa47d2a11a8b18111
duswnsxnxn <duswnsxnxn@gmail.com>
2024-04-02 00:45:22 +0900

    catalog 도메인 ì생성

---
+++ Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, produt_name, stock, unit_price)
+    values('CATALOG-001', 'Berlin', 100, 1500);
+insert into catalog(product_id, produt_name, stock, unit_price)
+    values('CATALOG-002', 'Tokyo', 100, 1000);
+insert into catalog(product_id, produt_name, stock, unit_price)
+    values('CATALOG-002', 'Stockholm', 100, 2000);
