9884cc3a164fbd9cd93c00d635ec04354a2faa8e
duswnsxnxn <duswnsxnxn@gmail.com>
2024-04-21 15:47:04 +0900

    - 사용하지 않는 import 정리

--- Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/vo/Greeting.java
+++ Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/vo/Greeting.java
@@ -1,9 +1,6 @@
 package com.example.userservice.vo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
