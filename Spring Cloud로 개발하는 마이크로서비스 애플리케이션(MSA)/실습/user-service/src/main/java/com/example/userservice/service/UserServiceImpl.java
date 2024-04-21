189ece662e486bfa05bdc6f0aefca494f14b09c1
duswnsxnxn <duswnsxnxn@gmail.com>
2024-04-21 15:38:21 +0900

    - findByEmail 메소드 추가 - 시큐리티에서 사용할 loadUserByUsername메소드 구현

--- Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA)/실습/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.userservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -53,5 +55,25 @@ public class UserServiceImpl implements UserService {
         return userRepository.findAll();
     }
 
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
 
+        if (userEntity == null)
+            throw new UsernameNotFoundException(email);
+
+        return UserDto.to_dto(userEntity);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
+
+        if (userEntity == null)
+            throw new UsernameNotFoundException(username + ": not found");
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>());
+    }
 }
